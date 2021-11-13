26c0a9b52d0c350098e82dcd7c03e7dc2bdc3d0e
Mike <nux7jr@gmail.com>
2021-11-13 17:56:49 +0700

    2.6
    Добавил popUp

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
+// Burger menu
 $(document).ready(function() {
-  $('burger-menu').click(function() {
-    $('menu').toggleClass('.menu--active')
-    console.log("click");
+  $('.burger-menu').click(function() {
+    $('.menu').toggleClass('menu--active');
+  });
+});
+// PopUp 
+$(document).ready(function() {
+  $('.btn-order').click(function() {
+    $('.order-window').addClass('order-window--active');
+  });
+  $('.btnOrder').click(function() {
+    $('.order-window').addClass('order-window--active');
+  });
+  $('.close-form-btn').click(function (){
+    $('.order-window').removeClass('order-window--active');
   })
+});
+
+// scroll
+// что-то с функцией
+(document).ready(function () {
+  $("#scrollDownBttn").click(function(){            
+    $('html, body').animate({
+            scrollDown: $("#pinkRegion").offset().top
+        }, 500);
+});
 })
-console.log("sadas")
