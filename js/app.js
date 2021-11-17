694b41a2863675d9a6fe6fdbcfca73b1bac9be95
Mike <nux7jr@gmail.com>
2021-11-17 16:44:51 +0700

    2.7
    Дописал функционал

--- js/app.js
+++ js/app.js
@@ -1,28 +1,49 @@
-// Burger menu
-$(document).ready(function() {
-  $('.burger-menu').click(function() {
-    $('.menu').toggleClass('menu--active');
-  });
-});
-// PopUp 
-$(document).ready(function() {
-  $('.btn-order').click(function() {
-    $('.order-window').addClass('order-window--active');
-  });
-  $('.btnOrder').click(function() {
-    $('.order-window').addClass('order-window--active');
-  });
-  $('.close-form-btn').click(function (){
-    $('.order-window').removeClass('order-window--active');
-  })
-});
+var menu = document.querySelector(".menu") 
+var popUpBtn = document.querySelector(".burger-menu")
 
-// scroll
-// что-то с функцией
-(document).ready(function () {
-  $("#scrollDownBttn").click(function(){            
-    $('html, body').animate({
-            scrollDown: $("#pinkRegion").offset().top
-        }, 500);
-});
+var orderBtn = document.querySelector(".btn-order")
+var orderWindow = document.querySelector(".order-window")
+var closeFormBtn = orderWindow.querySelector(".close-form-btn")
+
+var btnsOrder = document.querySelectorAll(".btnOrder"); // кпопка в card
+
+var moreInfoBnt = document.querySelector('.btn-more-info')
+var card = document.querySelector('.card')
+// burger menu
+
+popUpBtn.addEventListener("click", function(evt) {
+  evt.preventDefault;
+  menu.classList.toggle("menu--active");
+})
+
+// order window 
+
+// открытие формы
+// кпопка в меню
+orderBtn.addEventListener("click",function (evt) {
+  evt.preventDefault;
+  orderWindow.classList.add("order-window--active")
+})
+// кпопки в карточках
+for (var btnsOrders of btnsOrder) {
+  btnsOrders.addEventListener("click", changeClass)
+}
+function changeClass (orderWindow) {
+  var orderWindow = document.querySelector(".order-window");
+  orderWindow.classList.add("order-window--active")
+}
+
+// закрытие формы
+closeFormBtn.addEventListener("click",function (evt) {
+  evt.preventDefault;
+  orderWindow.classList.remove("order-window--active")
+})
+
+// scroll 
+moreInfoBnt.addEventListener("click", function() { 
+  var mainInfo = document.querySelector('.main-info-hr') 
+  mainInfo.scrollIntoView({
+    behavior: "smooth",
+    top: 2000
+  });
 })
