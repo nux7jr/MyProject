0893e88c929adf92201603b0d8d23b9db472c652
Mike <nux7jr@gmail.com>
2021-12-19 00:20:12 +0700

    2.9
    validForm

--- app/js/app.js
+++ app/js/app.js
@@ -1,49 +1,58 @@
-var menu = document.querySelector(".menu") 
-var popUpBtn = document.querySelector(".burger-menu")
+var menu = document.querySelector(".menu");
+var popUpBtn = document.querySelector(".burger-menu");
 
-var orderBtn = document.querySelector(".btn-order")
-var orderWindow = document.querySelector(".order-window")
-var closeFormBtn = orderWindow.querySelector(".close-form-btn")
+var orderBtn = document.querySelector(".btn-order");
+var orderWindow = document.querySelector(".order-window");
+var closeFormBtn = orderWindow.querySelector(".close-form-btn");
 
 var btnsOrder = document.querySelectorAll(".btnOrder"); // кпопка в card
 
-var moreInfoBnt = document.querySelector('.btn-more-info')
-var card = document.querySelector('.card')
+var moreInfoBnt = document.querySelector(".btn-more-info");
+var card = document.querySelector(".card");
+
+var formFocus = document.querySelector(".input-name");
 // burger menu
 
-popUpBtn.addEventListener("click", function(evt) {
+popUpBtn.addEventListener("click", function (evt) {
   evt.preventDefault;
   menu.classList.toggle("menu--active");
-})
+});
 
-// order window 
+// order window
 
 // открытие формы
+// фокус на форме
+function focusForm() {
+  formFocus.focus();
+}
 // кпопка в меню
-orderBtn.addEventListener("click",function (evt) {
+orderBtn.addEventListener("click", function (evt) {
   evt.preventDefault;
-  orderWindow.classList.add("order-window--active")
-})
+  orderWindow.classList.add("order-window--active");
+  focusForm();
+});
 // кпопки в карточках
 for (var btnsOrders of btnsOrder) {
-  btnsOrders.addEventListener("click", changeClass)
+  btnsOrders.addEventListener("click", changeClass);
 }
-function changeClass (orderWindow) {
+function changeClass(orderWindow) {
   var orderWindow = document.querySelector(".order-window");
-  orderWindow.classList.add("order-window--active")
+  orderWindow.classList.add("order-window--active");
+  focusForm();
 }
 
 // закрытие формы
-closeFormBtn.addEventListener("click",function (evt) {
+closeFormBtn.addEventListener("click", function (evt) {
   evt.preventDefault;
-  orderWindow.classList.remove("order-window--active")
-})
+  orderWindow.classList.remove("order-window--active");
+});
 
-// scroll 
-moreInfoBnt.addEventListener("click", function() { 
-  var mainInfo = document.querySelector('.main-info-hr') 
+// scroll
+moreInfoBnt.addEventListener("click", function () {
+  var mainInfo = document.querySelector(".main-info-hr");
   mainInfo.scrollIntoView({
     behavior: "smooth",
-    top: 2000
+    top: 2000,
   });
-})
+});
+//# sourceMappingURL=app.js.map
